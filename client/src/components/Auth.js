76ceb7a006fb37274aef94cdda9c07b9a7446b07
RishanRatna <rishanratna@hotmail.com>
2020-11-15 01:37:09 -0500

    Fixed transition issue on window resize, added unmounting animations on login/register, changed register form to support more fields

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -1,29 +1,71 @@
 import React, { useState } from 'react';
 import Login from './Login';
 import Register from './Register';
+import { CSSTransition } from 'react-transition-group'; 
 import '../css/Auth.css';
 
 const Auth = () => {
     const [loginRegister, setLoginRegister] = useState(false);
     let gradientDisplay;
+    let resizeTimer;
+
+    window.addEventListener("resize", () => {
+    document.body.classList.add("resize-animation-stopper");
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        document.body.classList.remove("resize-animation-stopper");
+    }, 400);
+    });
 
     const assignLoginRegister = (loginRegisterState) => {
         setLoginRegister(loginRegisterState);
     }
 
     if (loginRegister) {
-            gradientDisplay = <div className="gradient-display-background gradient-display-background-register"><div className='gradient-display gradient-display-register-rtrue'><h1 className='gradient-display-text'>Already have an account?</h1><button  onClick={() => setLoginRegister(false)} className='gradient-display-button gradient-display-button-red'>Login</button></div><div className='gradient-display gradient-display-register-bfalse'></div></div>;
+            gradientDisplay = ( <div className="gradient-display-background gradient-display-background-register">
+                                    <div className='gradient-display gradient-display-register-rtrue'>
+                                        <h1 className='gradient-display-text'>Already have an account?</h1>
+                                        <button  onClick={() => setLoginRegister(false)} className='gradient-display-button gradient-display-button-red'>Login</button>
+                                    </div>
+                                    <div className='gradient-display gradient-display-register-bfalse'></div>
+                                </div>);
     }
 
     else {
-            gradientDisplay = <div className="gradient-display-background gradient-display-background-login"><div className='gradient-display gradient-display-login-rfalse'></div><div className='gradient-display gradient-display-login-btrue'><h1 className='gradient-display-text'>Don't have an account?</h1><button onClick={() => setLoginRegister(true)} className='gradient-display-button gradient-display-button-blue'>Register</button></div></div>;
+            gradientDisplay = ( <div className="gradient-display-background gradient-display-background-login">
+                                    <div className='gradient-display gradient-display-login-rfalse'></div>
+                                    <div className='gradient-display gradient-display-login-btrue'>
+                                        <h1 className='gradient-display-text'>Don't have an account?</h1>
+                                        <button onClick={() => setLoginRegister(true)} className='gradient-display-button gradient-display-button-blue'>Register</button>
+                                    </div>
+                                </div>);
     }
 
     return (
         <div className="auth-container">
             {gradientDisplay}
-            <Register assignLoginReg={(loginRegisterState) => assignLoginRegister(loginRegisterState)}/>
-            <Login assignLoginReg={(loginRegisterState) => assignLoginRegister(loginRegisterState)}/>
+
+            <div className='fill'>
+                <CSSTransition
+                    in={loginRegister}
+                    timeout={500}
+                    classNames="reg-transition"
+                    unmountOnExit
+                >
+                    <Register assignLoginReg={(loginRegisterState) => assignLoginRegister(loginRegisterState)}/>
+                </CSSTransition>
+            </div>
+            <div className='fill'>
+                <CSSTransition
+                    in={!loginRegister}
+                    timeout={500}
+                    classNames="login-transition"
+                    unmountOnExit
+                >
+                    <Login assignLoginReg={(loginRegisterState) => assignLoginRegister(loginRegisterState)}/>
+                </CSSTransition>
+            </div>
+            
         </div>
     )
 }
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -5,26 +5,75 @@ const Register = (props) => {
     const [email, setEmail] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+
+    const [nameEmailPassword, setNameEmailPassword] = useState(0);
+
+    let regFormPage;
     
     
+    const handleNameNext = (e) => {
+        e.preventDefault();
+        setNameEmailPassword(1);
+        //Enter authentication code here to validate user credentials
+    }
+
+    const handleEmailNext = (e) => {
+        e.preventDefault();
+        setNameEmailPassword(2);
+        //Enter authentication code here to validate user credentials
+    }
+
     const handleSubmit = (e) => {
         e.preventDefault();
         //Enter authentication code here to validate user credentials
     }
 
+
+    if (nameEmailPassword === 0) {
+        regFormPage = (
+            <form className='register-form' onSubmit={handleNameNext}>
+                <input className='register-form-item register-input' type='text' placeholder='First Name' onChange={e => setFirstName(e.target.value)}/>
+                <input className='register-form-item register-input' type='text' placeholder='Last Name' onChange={e => setLastName(e.target.value)}/>
+                <div className='submission-buttons'>
+                    <button className='register-form-item register-button' type='submit'>Next</button>
+                </div>
+            </form>
+        );
+    }
+
+    else if (nameEmailPassword === 1) {
+        regFormPage = (
+            <form className='register-form' onSubmit={handleEmailNext}>
+                <input className='register-form-item register-input' type='text' placeholder='Email' onChange={e => setEmail(e.target.value)}/>
+                <div className='submission-buttons'>
+                    <button className='register-form-item register-button' type='button' onClick={() => setNameEmailPassword(0)}>Back</button>
+                    <button className='register-form-item register-button' type='submit'>Next</button>
+                </div>
+            </form>
+        );
+    }
+
+    else {
+        regFormPage = (
+            <form className='register-form' onSubmit={handleSubmit}>
+                <input className='register-form-item register-input' type='text' placeholder='Password' onChange={e => setPassword(e.target.value)}/>
+                <input className='register-form-item register-input' type='text' placeholder='Confirm Password' onChange={e => setConfirmPassword(e.target.value)}/>
+                <div className='submission-buttons'>
+                    <button className='register-form-item register-button' type='button' onClick={() => setNameEmailPassword(1)}>Back</button>
+                    <button className='register-form-item register-button' type='submit'>Sign up</button>
+                </div>
+            </form>
+        );
+    }
      //Password field should be secured and should not be openly updated in state
 
     return (
         <div className='register-container'>
             <h1 className='welcome-message'>Create account</h1>
-            <form className='register-form' onSubmit={handleSubmit}>
-                <input className='register-form-item register-input' type='text' placeholder='Email' onChange={e => setEmail(e.target.value)}/>
-                <input className='register-form-item register-input' type='text' placeholder='Username' onChange={e => setUsername(e.target.value)}/>
-                <input className='register-form-item register-input' type='text' placeholder='Password' onChange={e => setPassword(e.target.value)}/>
-                <input className='register-form-item register-input' type='text' placeholder='Confirm Password' onChange={e => setConfirmPassword(e.target.value)}/>
-                <button className='register-form-item register-button' type='submit'>Sign up</button>
-            </form>
+            {regFormPage}
         </div>
     )
 }
